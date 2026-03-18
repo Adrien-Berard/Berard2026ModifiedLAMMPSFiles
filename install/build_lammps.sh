fd6bdd71ad73a239fe36098956a2b93b4ed85200
Adrien Berard <74345091+Adrien-Berard@users.noreply.github.com>
2026-03-18 08:37:45 +0100

    Update build_lammps.sh to replace fix_bond_react files
    Remove old fix_bond_react files and download modified versions.

--- install/build_lammps.sh
+++ install/build_lammps.sh
@@ -1,15 +1,17 @@
+rm -rf modified_lammps_Apr2024
+
 git clone https://github.com/lammps/lammps.git modified_lammps_Apr2024
-cd modified_lammps_Apr2024/lammps
+cd modified_lammps_Apr2024
 
 git checkout patch_17Apr2024
 
 rm -rf build
-rm src/REACTION/fix_bond_react.cpp src/REACTION/fix_bond_react.h
+rm -f src/REACTION/fix_bond_react.cpp
+rm -f src/REACTION/fix_bond_react.h
 
+wget https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/main/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.cpp -P src/REACTION/
 
-
-cp https://github.com/adrien-berard/modified_lammps_reacter/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.cpp src/REACTION/
-cp https://github.com/adrien-berard/modified_lammps_reacter/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.h src/REACTION/
+wget https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/main/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.h -P src/REACTION/
 
 mkdir build
 cd build
