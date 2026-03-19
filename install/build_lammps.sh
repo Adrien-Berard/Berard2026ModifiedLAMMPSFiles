cd00eb9e594641a564e971373759e97b7616422f
Adrien Berard <74345091+Adrien-Berard@users.noreply.github.com>
2026-03-19 14:28:14 +0100

    Enhance build_lammps.sh with error handling and updates
    Updated the build script to include error handling, improved file download commands, and organized USER-LE package downloads.

--- install/build_lammps.sh
+++ install/build_lammps.sh
@@ -1,32 +1,66 @@
 rm -rf modified_lammps_Apr2024
 
+#!/usr/bin/env bash
+set -e  # stop on error
+
+# --- Clone LAMMPS ---
 git clone https://github.com/lammps/lammps.git modified_lammps_Apr2024
 cd modified_lammps_Apr2024
 
 git checkout patch_17Apr2024
 
+# --- Clean previous builds ---
 rm -rf build
+
+# --- Replace REACTION fix ---
 rm -f src/REACTION/fix_bond_react.cpp
 rm -f src/REACTION/fix_bond_react.h
 
-wget https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/master/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.cpp -P src/REACTION/
-wget https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/master/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.h -P src/REACTION/
+wget -q https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/master/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.cpp -O src/REACTION/fix_bond_react.cpp
+wget -q https://raw.githubusercontent.com/adrien-berard/modified_lammps_reacter/master/modified_lammps_Apr2024/fix_bond_react_modified_version/fix_bond_react.h -O src/REACTION/fix_bond_react.h
 
-# Build for loop extrusion module (to comment if not using)
-wget https://raw.githubusercontent.com/polly-code/lammps_le/tree/6e6c3cf461ca75c3755cc7fdf7c9891934d69dc7/src/USER-LE -P src/
+# --- USER-LE package (loop extrusion) ---
+# NOTE: must download actual files, not directory
 
+rm -f src/compute_cb.cpp
+rm -f src/compute_cb.h
+
+wget https://raw.githubusercontent.com/polly-code/lammps_le/main/src/compute_cb.cpp
+wget https://raw.githubusercontent.com/polly-code/lammps_le/main/src/compute_cb.h
+
+mkdir -p src/USER-LE
+
+base_url="https://raw.githubusercontent.com/polly-code/lammps_le/main/src/USER-LE"
+
+files=(
+  fix_ex_load.cpp
+  fix_ex_load.cpp
+  fix_ex_load.cpp
+  fix_ex_load.cpp
+  fix_extrusion.cpp
+  fix_extrusion.h
+)
+
+for f in "${files[@]}"; do
+  wget -q "${base_url}/${f}" -O "src/USER-LE/${f}"
+done
+
+# --- Build ---
 mkdir build
 cd build
 
 cmake -C ../cmake/presets/most.cmake \
       -C ../cmake/presets/nolib.cmake \
+      -D BUILD_MPI=ON \
       -D PKG_REACTION=ON \
       -D PKG_MOLECULE=ON \
-      -D -D PKG_MISC=ON \
-      -D PKG_USER-LE=ON \
+      -D PKG_MISC=ON \
       -D PKG_MPIIO=ON \
-      -D PKG_MOLECULE=ON \
-      -D PKG_MC=ON
+      -D PKG_MC=ON \
+      -D LAMMPS_EXTRA_SOURCE_DIR=../src/USER-LE \
       ../cmake
 
-make -j$(nproc)
+cmake --build . -j$(nproc)
+
+# --- Verify ---
+./lmp -h | grep USER
