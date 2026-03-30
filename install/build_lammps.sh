884d502dcf09a47e73e72ff1e10dff1d1b091c5e
Adrien Berard <74345091+Adrien-Berard@users.noreply.github.com>
2026-03-30 13:26:47 +0200

    Remove USER-LE package installation from build script
    Removed USER-LE package download and related files from the build script.

--- install/build_lammps.sh
+++ install/build_lammps.sh
@@ -22,27 +22,6 @@ base_react="https://raw.githubusercontent.com/Adrien-Berard/Berard2026ModifiedLA
 wget -q "${base_react}/fix_bond_react.cpp" -O src/REACTION/fix_bond_react.cpp
 wget -q "${base_react}/fix_bond_react.h"   -O src/REACTION/fix_bond_react.h
 
-# --- USER-LE package (loop extrusion) ---
-wget -q https://raw.githubusercontent.com/polly-code/lammps_le/main/src/compute_cb.cpp \
-     -O src/compute_cb.cpp
-wget -q https://raw.githubusercontent.com/polly-code/lammps_le/main/src/compute_cb.h \
-     -O src/compute_cb.h
-
-mkdir -p src/USER-LE
-
-
-files=(
-  fix_ex_load.cpp
-  fix_ex_load.h
-  fix_ex_unload.cpp
-  fix_ex_unload.h
-  fix_extrusion.cpp
-  fix_extrusion.h
-)
-
-for f in "${files[@]}"; do
-  wget -q "https://raw.githubusercontent.com/polly-code/lammps_le/main/src/USER-LE/${f}" -O "src/USER-LE/${f}"
-done
 
 # --- Build ---
 mkdir build
@@ -53,16 +32,12 @@ cmake -C ../cmake/presets/most.cmake \
       -D BUILD_MPI=ON \
       -D PKG_REACTION=ON \
       -D PKG_MOLECULE=ON \
-      -D PKG_MISC=ON \
-      -D PKG_MPIIO=ON \
-      -D PKG_MC=ON \
-      -D LAMMPS_EXTRA_SOURCE_DIR=../src/USER-LE \
       ../cmake
 
 cmake --build . -j$(nproc)
 
 # --- Verify ---
-./lmp -h | grep USER
+./lmp -h
 
 # --- Add lmp to PATH ---
 echo "export PATH=\$PATH:/home/adrien/modified_lammps_Apr2024/build" >> ~/.bashrc  # Change your path accordingly
