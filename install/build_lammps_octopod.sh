b3cbe179b71e71a4442fcf89206af5ade8c9dd69
Adrien Berard <74345091+Adrien-Berard@users.noreply.github.com>
2026-04-02 16:22:55 +0200

    Update build_lammps_octopod.sh

--- install/build_lammps_octopod.sh
+++ install/build_lammps_octopod.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
-set -e  # stop on error
-set -x  # print every command before executing it
-
-# --- Clone LAMMPS at the specific tag ---
-# rm -rf modified_lammps_Apr2024
-trash modified_lammps_Apr2024
-
-git clone --depth 1 --branch patch_17Apr2024 \
-    https://github.com/lammps/lammps.git modified_lammps_Apr2024
-
-cd modified_lammps_Apr2024
-git switch -c my_modified_lammps
 
 # --- Clean previous builds ---
 # rm -rf build
@@ -22,37 +9,59 @@ trash build
 trash src/REACTION/fix_bond_react.cpp
 trash src/REACTION/fix_bond_react.h
 
-base_react="https://raw.githubusercontent.com/Adrien-Berard/Berard2026ModifiedLAMMPSFiles/master/fix_bond_react_modified_version"
-wget -q "${base_react}/fix_bond_react.cpp" -O src/REACTION/fix_bond_react.cpp
-wget -q "${base_react}/fix_bond_react.h"   -O src/REACTION/fix_bond_react.h
+---
 
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+# --- Load modules (adjust names) ---
+module load nbi intel-oneapi-mpi/2023.0 mkl/2023.0.0
+module load nbi intel-oneapi-mpi/2023.0 mkl/2023.0.0 mpi/2021.8.0
+
+# --- Paths ---
+INSTALL_DIR="${HOME}/modified_lammps_Apr2024"
+SCRATCH_BUILD="${SCRATCH:-$HOME}/lammps"
+
+# --- Clone ---
+rm -rf "$INSTALL_DIR"
+git clone --depth 1 --branch patch_17Apr2024 \
+    https://github.com/lammps/lammps.git "$INSTALL_DIR"
+cd "$INSTALL_DIR"
+git switch -c my_modified_lammps
+
+# --- Patch REACTION fix ---
+rm -f src/REACTION/fix_bond_react.cpp src/REACTION/fix_bond_react.h
+BASE_REACT="https://raw.githubusercontent.com/Adrien-Berard/Berard2026ModifiedLAMMPSFiles/master/fix_bond_react_modified_version"
+wget -q "${BASE_REACT}/fix_bond_react.cpp" -O src/REACTION/fix_bond_react.cpp
+wget -q "${BASE_REACT}/fix_bond_react.h"   -O src/REACTION/fix_bond_react.h
 
 # --- Build ---
-mkdir build
-cd build
+rm -rf "$SCRATCH_BUILD"
+mkdir -p "$SCRATCH_BUILD"
+cd "$SCRATCH_BUILD"
 
-cmake -C ../cmake/presets/most.cmake \
-      -C ../cmake/presets/nolib.cmake \
-      cmake ../cmake \
+cmake -C "$INSTALL_DIR/cmake/presets/most.cmake" \
+      -C "$INSTALL_DIR/cmake/presets/nolib.cmake" \
       -D BUILD_MPI=ON \
-      -D CMAKE_CXX_COMPILER=/nbi/software/intel-oneapi/2023.0.0.25537/mpi/2021.8.0/bin/mpicxx \
+      -D CMAKE_CXX_COMPILER=mpicxx \
       -D BUILD_OMP=ON \
       -D PKG_KOKKOS=ON \
       -D Kokkos_ENABLE_OPENMP=ON \
       -D Kokkos_ARCH_ZEN3=ON \
       -D FFT_KOKKOS=MKL \
-      -D CMAKE_BUILD_TYPE=Release
+      -D CMAKE_BUILD_TYPE=Release \
       -D PKG_REACTION=ON \
       -D PKG_MOLECULE=ON \
-      ../cmake
+      "$INSTALL_DIR/cmake"
 
 cmake --build . -j 16
 
 # --- Verify ---
 ./lmp -h
 
-# --- Add lmp to PATH ---
-echo "export PATH=\$PATH:/home/adrien/modified_lammps_Apr2024/build" >> ~/.bashrc  # Change your path accordingly
-source ~/.bashrc
+# --- Add to PATH (idempotent) ---
+EXPORT_LINE="export PATH=\$PATH:${SCRATCH_BUILD}"
+grep -qxF "$EXPORT_LINE" ${HOME}/.bashrc || echo "$EXPORT_LINE" >> ${HOME}/.bashrc
+source ${HOME}/.bashrc
 lmp -h
-
